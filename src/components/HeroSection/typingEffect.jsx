e128a4a9ceecbfabe06271135bd60f108c3ca853
SUNNYKUMAR1232 <123469525+SUNNYKUMAR1232@users.noreply.github.com>
2024-02-11 20:32:03 +0530

    faet: add some style in hero section

--- src/components/HeroSection/typingEffect.jsx
+++ src/components/HeroSection/typingEffect.jsx
@@ -1,16 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 const TypingEffect = () => {
   return (
     <motion.div
-    animate={TypingEffectStyle.animate}
-    initial={TypingEffectStyle.initial}
-    transition={TypingEffectStyle.transition}
+      animate={TypingEffectStyle.animate}
+      initial={TypingEffectStyle.initial}
+      transition={TypingEffectStyle.transition}
     >
       <Typography variant="h1">
         <span style={TypingEffectStyle.text1}>Hi</span>
-        , <br></br>I&apos;m{" "}
+        <span style={TypingEffectStyle.text3}>,</span> <br></br>
+        <span style={TypingEffectStyle.text2}>I&apos;m </span>
         <TypeAnimation
           sequence={["Sunny", 1000, " ", 1000]}
           wrapper="span"
@@ -34,34 +35,45 @@ export default TypingEffect;
 const TypingEffectStyle = {
   Into: {
     background: "linear-gradient(145deg, #0afb02, #3007ff)",
-    WebkitBackgroundClip:'text',
-    WebkitTextFillColor:'transparent',
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
   },
   Skill: {
     fontSize: "2.5em",
     display: "inline-block",
     background: "linear-gradient(145deg, #02f3fb, #ff0707)",
-    WebkitBackgroundClip:'text',
-    WebkitTextFillColor:'transparent',
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
   },
-  animate:{
-    opacity:1,
-    y:0,
-    
+  animate: {
+    opacity: 1,
+    y: 0,
   },
-  initial:{
-    opacity:0,
-    y:-20,
+  initial: {
+    opacity: 0,
+    y: -20,
   },
-  transition:{
-    duration:1,
-    damping:20,
-    ease:"easeInOut",
+  transition: {
+    duration: 1,
+    damping: 20,
+    ease: "easeInOut",
   },
-  text1:{
-    color:'color-mix(in srgb-linear, #17fb02  , #2705d1 50%)',
-    background:'linear-gradient(145deg, #fb0202, #3007ff)',
-    WebkitBackgroundClip:'text',
-    WebkitTextFillColor:'transparent',
-  }
+  text1: {
+    color: "color-mix(in srgb-linear, #17fb02  , #2705d1 50%)",
+    background: "linear-gradient(145deg, #fb0202, #3007ff)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  },
+  text2: {
+    color: "color-mix(in srgb-linear, #17fb02  , #2705d1 50%)",
+    background: "linear-gradient(145deg, #fb0202, #3007ff)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  },
+  text3: {
+    color: "color-mix(in srgb-linear, #17fb02  , #2705d1 50%)",
+    background: "linear-gradient(145deg, #fb0202, #3007ff)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  },
 };
