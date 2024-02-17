9ba03a3b3dba42249fe18f1f5976204b3b7c6b85
SUNNYKUMAR1232 <123469525+SUNNYKUMAR1232@users.noreply.github.com>
2024-02-17 19:56:48 +0530

    feat:  update not found page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,22 @@
-'use client'
-import About from '@/components/AboutSection/About'
-import Experience from '@/components/Experience/Experience'
-import Hero from '@/components/HeroSection/Hero'
-import NavBar from '@/components/navBar/NavBar'
-import GetTouch from '@/components/GetTouch/GetTouch'
-import Project from '@/components/Project/Project'
-import Education from '@/components/Education/Education'
-import Footer from '@/components/Footer/Footer'
-import Skill from '@/components/Skills/Skill'
-import { Box } from '@mui/material'
+"use client";
+import About from "@/components/AboutSection/About";
+import Experience from "@/components/Experience/Experience";
+import Hero from "@/components/HeroSection/Hero";
+import NavBar from "@/components/navBar/NavBar";
+import GetTouch from "@/components/GetTouch/GetTouch";
+import Project from "@/components/Project/Project";
+import Education from "@/components/Education/Education";
+import Footer from "@/components/Footer/Footer";
+import Skill from "@/components/Skills/Skill";
+import { Box } from "@mui/material";
+import Imagekit from "@/features/imagekit";
 export default function Home() {
   return (
-    <main >
-      <Box >
+    <main>
+      <Box>
         <NavBar />
       </Box>
       <Hero />
     </main>
-  )
+  );
 }
